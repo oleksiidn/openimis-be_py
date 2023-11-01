2f64ba709c972b8cc4b2ed713530b5304b68f25c
oleksiidn <oleksii.danyliuk@gofore.com>
2023-11-01 14:07:08 +0200

    Hack for running in the sandbox

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -16,47 +16,13 @@ show_help() {
 }
 
 #export PYTHONPATH="/opt/app:$PYTHONPATH"
-if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
-  export DJANGO_SETTINGS_MODULE=openIMIS.settings
-fi
 
-case "$1" in
-  "start" )
-    if [ "${DJANGO_MIGRATE,,}" == "true" ] || [ "${SITE_ROOT}" == "api" ]
-    then
-      echo "Migrating..."
-      python manage.py migrate
-    fi
-    echo "Starting Django..."
-    SCHEDULER_AUTOSTART=True python server.py
-  ;;
-  "start_asgi" )
-    echo "Starting Django ASGI..."
-    def_ip='0.0.0.0'
-    def_port='8000'
-    def_app='openIMIS.asgi:application'
 
-    SERVER_IP="${ASGI_IP:-$def_ip}"
-    SERVER_PORT="${ASGI_PORT:-$def_port}"
-    SERVER_APPLICATION="${ASGI_APPLICATION:-$def_app}"
+  cd ../openIMIS
+  echo pwd
 
-    daphne -b "$SERVER_IP" -p "$SERVER_PORT" "$SERVER_APPLICATION"
-  ;;
-  "worker" )
-    echo "Starting Celery with url ${CELERY_BROKER_URL} ${DB_NAME}..."
-    echo "Settings module: $DJANGO_SETTINGS_MODULE"
-    celery -A openIMIS worker --loglevel=DEBUG
-  ;;
-  "manage" )
-    ./manage.py "${@:2}"
-  ;;
-  "eval" )
-    eval "${@:2}"
-  ;;
-  "bash" )
-    bash
-  ;;
-  * )
-    show_help
-  ;;
-esac
+  echo "Starting Django..."
+
+  python manage.py runserver 0.0.0.0:8000
+
+#  =True python server.py
